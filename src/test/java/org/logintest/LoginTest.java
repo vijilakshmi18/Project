5c0ccecdff269b2613e6166ca648808ca548f094
vijilakshmi18 <vijipandiyan18@gmail.com>
2021-11-29 10:55:48 -0800

    lakshmi's code

--- src/test/java/org/logintest/LoginTest.java
+++ src/test/java/org/logintest/LoginTest.java
@@ -1,5 +1,14 @@
 package org.logintest;
 
 public class LoginTest {
-
+	public static void main(String[] args) {
+		
+	for(int row=1;row<=3;row++) {                 
+		for (int col=1;col<=3;col++) {            
+			System.out.print( col );              
+		}
+	System.out.println();
+	}
+	
+	}
 }
